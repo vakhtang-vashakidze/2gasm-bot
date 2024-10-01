864ccf80e2dd381fea1b0fb11aa8d11dfbc0daa5
vakhtang-vashakidze <vvash2017@gmail.com>
2024-10-01 16:50:25 +0400

    updated application config and main bot itself

--- src/main/java/ge/vakhtang/rgsm/bots/MainBot.java
+++ src/main/java/ge/vakhtang/rgsm/bots/MainBot.java
@@ -1,7 +1,6 @@
 package ge.vakhtang.rgsm.bots;
 
 import ge.vakhtang.rgsm.service.DialogService;
-import ge.vakhtang.rgsm.service.MusicService;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -11,11 +10,9 @@ import org.springframework.stereotype.Component;
 public class MainBot extends ListenerAdapter {
 
     private final DialogService dialogService;
-    private final MusicService musicService;
 
-    public MainBot(DialogService dialogService, MusicService musicService) {
+    public MainBot(DialogService dialogService) {
         this.dialogService = dialogService;
-        this.musicService = musicService;
     }
 
     @Override
